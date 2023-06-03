0da1efda66d55f093d4f2fd3bb98d67e13cda958
Muanester <stevenmutisya999@gmail.com>
2023-06-03 16:19:07 +0300

    shop_page

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Navbar and Close 
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
@@ -12,4 +13,21 @@ if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active')
     })
+}
+
+// Small image changing
+var Mainimg = document.getElementById("Mainimg");
+var Smallimg = document.getElementsByClassName("small-img");
+
+Smallimg[0].onclick = function() {
+    Mainimg.src = Smallimg[0].src;
+}
+Smallimg[1].onclick = function() {
+    Mainimg.src = Smallimg[1].src;
+}
+Smallimg[2].onclick = function() {
+    Mainimg.src = Smallimg[2].src;
+}
+Smallimg[3].onclick = function() {
+    Mainimg.src = Smallimg[3].src;
 }
